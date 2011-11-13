8360f65df204b5935045c4b9bc873437117dd1ed
Mischa <mischavogt@gmx.de>
2011-11-13 21:35:13 +0100

    TemplateSelector added to create different context menus

---
+++ RW-Ligamodus/Teammanager.View/TemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using Teammanager.Core;
+
+namespace Teammanager.View
+{
+    public class TemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var team = item as Team;
+            var member = item as TeamMember;
+
+            if((team == null) && (member == null))
+            {
+                return base.SelectTemplate(item, container);
+            }
+
+            if (team != null)
+            {
+                return TeamTemplate;
+            }
+            else
+            {
+                return TeamMemberTemplate;
+            }
+        }
+
+
+        #region properties
+
+        public DataTemplate TeamMemberTemplate { get; set; }
+        public DataTemplate TeamTemplate { get; set; }
+
+        #endregion
+
+    }
+}
